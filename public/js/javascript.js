2c18a61f6fd88cfd2a459efa1fbe13b4cd84ed1f
alphaborel <aaborel@gmail.com>
2018-05-13 17:15:42 -0700

    Well I think I can call this mostly done

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,41 +1,143 @@
-
 function submitNewUsr() {
-//get the values of the form
-let user = document.getElementById('username');
-let email = document.getElementById('email');
-let state = document.getElementById('state');
+  //get the values of the form
+  let user = document.getElementById('username');
+  let email = document.getElementById('email');
+  let state = document.getElementById('state');
 
-//create user object
-let usrObj = {
-  'name' : user.value,
-  'email' : email.value,
-  'state' : state.value
-}
+  //create user object
+  let usrObj = {
+    'name': user.value,
+    'email': email.value,
+    'state': state.value
+  }
 
-// send over user object
-fetch("/user/add/",
-{
-  headers: {
-    'content-type' : 'application/json'
-  },
-  method: 'POST',
-  body: JSON.stringify(usrObj)
-})
-.then(function(res){ alert("User Created!"); console.log(res) })
-.catch(function(res){ alert("Something Went Wrong!"); console.log(res) })
+  // send over user object
+  fetch("/user/add/", {
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'PUT',
+      body: JSON.stringify(usrObj)
+    })
+    .then(function(res) {
+      alert("User Created!");
+      console.log(res)
+    })
+    .catch(function(res) {
+      alert("Something Went Wrong!");
+      console.log(res)
+    })
+
+  //clear text boxes
+  user.value = "";
+  email.value = "";
+  state.value = "";
+
 }; //end submitNewUsr()
 
 //request a single user
-function userSearch(){
+function userSearch() {
   let bigTextBox = document.getElementById('textfield');
   let usernameSearch = document.getElementById('nameSearch');
-  fetch(`/user/${usernameSearch.value}`
-)
-  .then(function(res){ res.text().then(function(text) {
-    bigTextBox.textContent = text; })
-  })
-  .catch(function(res){ alert("Something Went Wrong!"); console.log(res) })
-} // end userSearch()
+  fetch(`/user/${usernameSearch.value}`)
+    .then(function(res) {
+      let singleUsr = res.text();
+      singleUsr.then(function(value) {
+
+        if (value !== "User not found") {
+          //turning the text returned into an object
+          let output = JSON.parse(value);
+          //populating the text boxes with the rest of the object
+          document.getElementById('emailSearch').value = output.email;
+          document.getElementById('stateSearch').value = output.state;
+          return;
+        } //end if statement
+
+        alert("User not found");
+      }) //end second then function
+    })
+    .catch(function(res) {
+      alert("Something Went Wrong!");
+      console.log(res)
+    })
+}; // end userSearch()
+
+//grabbing elements for the update user click event
+var userUpdateBtn = document.getElementById('updateBtn');
+
+userUpdateBtn.addEventListener('click', function() {
+  //tried queryselectorall here but it wouldn't work with .value ?
+  switch (true) {
+    case document.getElementById('nameSearch').value === "":
+      alert("Please Seach for a user before updating.")
+      break;
+    case document.getElementById('emailSearch').value === "":
+      alert("Please Seach for a user before updating.")
+      break;
+    case document.getElementById('stateSearch').value === "":
+      alert("Please Seach for a user before updating.")
+      break;
+  }; //end switch statement
+
+  //create updated user object
+  let updatedUsrObj = {
+    'name': nameSearch.value,
+    'email': emailSearch.value,
+    'state': stateSearch.value
+  }
+
+  //check to make sure the user object doesn't have any blanks
+  if (updatedUsrObj.name === "" || updatedUsrObj.email === "" || updatedUsrObj.state === "") {
+    return;
+  }
+
+  fetch("./user/update/", {
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'PUT',
+      body: JSON.stringify(updatedUsrObj)
+    })
+    .then(function(res) {
+      alert("User Updated!");
+    })
+    .catch(function(res) {
+      alert("Something Went Wrong!");
+      console.log(res)
+    })
+
+  //clear text boxes
+  nameSearch.value = "";
+  emailSearch.value = "";
+  stateSearch.value = "";
+
+}) //end update user eventlistener
+
+function deleteUser() {
+  let usrnameToDel = document.getElementById('usrNameBox').value;
+  fetch(`/user/del/${usrnameToDel}`, {
+      headers: {
+        'content-type': 'text/plain'
+      },
+      method: 'DELETE',
+    })
+    .then(function(res) {
+      let serverRes = res.text();
+      serverRes.then(function(value) {
+        //check and see if the delete was sucessful
+        if (value === "User not found") {
+          alert("User not found!");
+          return;
+        } //end if statement
+        alert("User deleted")
+      })
+    })
+    .catch(function(res) {
+      alert("Something Went Wrong!");
+      console.log(res)
+    })
+
+}; //end deleteUser()
 
 //grabbing elements for the search all click event
 var returnAll = document.getElementById('searchAllBtn');
@@ -43,8 +145,13 @@ var returnAll = document.getElementById('searchAllBtn');
 returnAll.addEventListener('click', function() {
   let allUsrTextBox = document.getElementById('textfield');
   fetch("/user/all")
-  .then(function(res){ res.text().then(function(text) {
-    allUsrTextBox.textContent = text; })
-  })
-  .catch(function(res){ alert("Something Went Wrong!"); console.log(res) })
-});//end returnAll event listener and seach all btn
+    .then(function(res) {
+      res.text().then(function(text) {
+        allUsrTextBox.textContent = text;
+      })
+    })
+    .catch(function(res) {
+      alert("Something Went Wrong!");
+      console.log(res)
+    })
+}); //end returnAll event listener and seach all btn
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("", function(req, res) {
 });
 
 //this is the interface to add a new user
-app.post("/user/add/", function(req, res) {
+app.post("/user/add", function(req, res) {
   let usrInput = req.body;
   let readArr = fs.readFileSync("./storage.json", "utf8");
   let newArr = JSON.parse(readArr);
@@ -30,6 +30,24 @@ app.post("/user/add/", function(req, res) {
   fs.writeFileSync('./storage.json', JSON.stringify(newArr));
 
   res.send('The user was appended to file!');
+});
+
+//this is the rout to update an existing user
+app.put("/user/update", function(req, res) {
+  let updatedUsr = req.body;
+
+  //read the "database" and make it into an array
+  let usrList = fs.readFileSync("./storage.json", "utf8");
+  let usrListArr = JSON.parse(usrList);
+
+  //filter out the existing user object
+  let minusUsr = usrListArr.filter(update => update.name !== updatedUsr.name);
+
+  minusUsr.push(updatedUsr);
+
+  fs.writeFileSync('./storage.json', JSON.stringify(minusUsr));
+
+  res.sendStatus(200);
 });
 
 //this is the route for returning all users
@@ -50,9 +68,28 @@ app.get("/user/:name", function(req, res) {
   for (let i = 0; i < userArr.length; i++) {
     if (userArr[i].name === userToFind) {
       return res.send(userArr[i]);
-        } // end if statement
-      } //end for loop
-      res.send("User not found");
+    } // end if statement
+  } //end for loop
+  res.send("User not found");
+});
+
+app.delete("/user/del/:user", function(req, res) {
+  let usrToDel = req.params.user;
+
+  //read the "database" and make it into an array
+  let usrListDel = fs.readFileSync("./storage.json", "utf8");
+  let usrListArrDel = JSON.parse(usrListDel);
+
+  //filter out the existing user object
+  let deletedUsr = usrListArrDel.filter(find => find.name !== usrToDel);
+
+  if(usrListArrDel.length === deletedUsr.length){
+    res.send("User not found");
+  } else {
+    fs.writeFileSync('./storage.json', JSON.stringify(deletedUsr));
+    res.sendStatus(200);
+  }
+
 });
 
 app.use(function(req, res) {
